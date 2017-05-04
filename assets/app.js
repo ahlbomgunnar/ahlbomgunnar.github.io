dd07c22493bc47a45795880cc4f2bb4920b7db6e
Gunnar Ahlbom <Gahlbom@gmail.com>
2017-05-04 13:42:39 +0200

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,7 @@ var datalist = [
         date:'May 3rd 2017',
         src :'assets/img/orbit.png',
         quote:'A Three.js 3D tech demo',
-        filler:['Orbit is a solarsystem simulation project in JavaScript built with the Three.js library, which extends the regular 'canvas' element to make use of the computers GPU.',
+        filler:['Orbit is a solarsystem simulation project in JavaScript built with the Three.js library, which extends the regular canvas element to make use of the computers GPU.',
 		'This means that a couple hundred objects can be rendered in three dimensions at 60 frames per second with relative ease. This project demonstrates this rendering capacity, and shows what can be accomplished with a modern web browser.'],
         technologies:['JavaScript', 'Three.js'],
         projectImgOne:'assets/img/orbit-web.png',
